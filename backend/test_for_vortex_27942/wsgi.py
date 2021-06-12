2a23ba1730574c52a6fbbda704b5a8d88e7fc304
Crowdbotics <team@crowdbotics.com>
2021-06-12 17:30:14 +0000

    Update models

--- backend/test_for_vortex_27942/wsgi.py
+++ backend/test_for_vortex_27942/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_for_vortex_27942.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_for_vortex_27942.settings")
 
 application = get_wsgi_application()
